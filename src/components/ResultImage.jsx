571e94e87e63890f530199617b66dd7aecbbc396
unknown <egor@westsib.ru>
2023-11-14 22:46:40 +0700

    asd'flas

--- src/components/ResultImage.jsx
+++ src/components/ResultImage.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
 const ResultImage = () => {
-  return <div>Image for result</div>
+  const addResultImgUrl = (e) => {
+    console.log('!!!')
+  }
+
+  return (
+    <div>
+      <div className="image-holder">
+        <img
+          className="question-img"
+          src={'imgSrc'}
+          alt={`Вы пытались загрузить картинку по ссылке {источник картинки}`}
+        />
+      </div>
+      <p>URL</p>
+      <form>
+        <input type="text" name="resultPictureUrl" />
+        <button className="addResultImageUrl" onClick={addResultImgUrl}>
+          {' '}
+          Добавить{' '}
+        </button>
+      </form>
+    </div>
+  )
 }
 
 export default ResultImage
